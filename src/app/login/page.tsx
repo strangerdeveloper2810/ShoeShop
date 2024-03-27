1ad9da6b6c52c38a3ceb6c08f4a844e6677e7e7b
Nguyễn Hải Trình <nguyenmaster3012@gmail.com>
2024-03-27 17:36:26 +0700

    fix href register in page Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -150,7 +150,7 @@ const Login: React.FC = () => {
                     </Link>
                   </Grid>
                   <Grid item>
-                    <Link href="#" variant="body2">
+                    <Link href={"/register"} variant="body2">
                       {"Don't have an account? Sign Up"}
                     </Link>
                   </Grid>
